2bb21ef428ba82667e8be698a1bd0ca37fed6dc4
Jessie Chatham Spencer <jessie@teainspace.com>
2023-09-24 09:38:55 +0200

    Shit seems to work

--- shell.nix
+++ shell.nix
@@ -1,11 +1,43 @@
 { pkgs ? import <nixpkgs> {} }:
-(pkgs.buildFHSUserEnv {
-  name = "pythondev";
-  targetPkgs = pkgs: (with pkgs; [
-    gcc
-    binutils-unwrapped
-    rustup
-  ]);
-  runScript = "bash";
-}).env
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ rustup cargo gcc ];
+  buildInputs = with pkgs; [ 
+    clang 
+    rustfmt 
+    clippy 
+    just 
+    kube3d 
+    openssl 
+    pkgconfig 
+    python310Packages.ipython
+    protobuf
+    kustomize
+    kubectl
+    operator-sdk
+    postgresql
+    s3cmd
+    minio-client
+    argo
+    kubernetes-helm
+    grpcurl
+    sass
+    rust-analyzer
+    jq
+    mkdocs
+    python310Packages.mkdocs-material
+    ripgrep
+  ];
 
+  # Certain Rust tools won't work without this
+  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  # This is required to build hun-spell for cargo-spellchecker
+  LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"; 
+
+  # TODO: use cargo bin path env variables instead of specific path.
+  shellHook = ''
+    export PATH=$PATH:~/.cargo/bin
+  '';
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,91 @@
-use std::{ffi::OsStr, path::Path};
+use std::{
+    cmp::min,
+    collections::HashMap,
+    ffi::OsStr,
+    os::unix::process::CommandExt,
+    path::{self, Path, PathBuf},
+    process,
+};
 
+use clap::{Command, Parser};
+use dialoguer::Select;
 use grep::{
     regex::RegexMatcher,
     searcher::{sinks::UTF8, Searcher, SearcherBuilder},
 };
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser)]
+struct Cli {
+    test: String,
+    term: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+struct CliData {
+    prev_tests: HashMap<String, String>,
+}
+
+fn app_data_path() -> Option<PathBuf> {
+    directories::ProjectDirs::from("", "jmintb", "cargotst").map(|p| p.data_dir().to_path_buf())
+}
+
+fn app_data_file_path() -> Option<PathBuf> {
+    app_data_path().map(|p| {
+        let mut p = p;
+        p.push("app_data.json");
+        p
+    })
+}
+
+impl CliData {
+    fn init() -> Self {
+        if let Ok(data) = serde_json::from_slice(
+            std::fs::read(app_data_file_path().unwrap())
+                .unwrap_or(vec![])
+                .as_slice(),
+        ) {
+            data
+        } else {
+            Self::default()
+        }
+    }
+
+    fn save_test(mut self, project_path: String, test_name: String) {
+        self.prev_tests.insert(project_path, test_name);
+        println!("dir: {:?}", app_data_file_path().unwrap());
+        std::fs::create_dir_all(app_data_path().unwrap());
+
+        std::fs::write(
+            app_data_file_path().unwrap(),
+            serde_json::to_string(&self).unwrap(),
+        )
+        .unwrap();
+    }
+
+    fn get_last_test(&self, project_path: String) -> Option<String> {
+        println!("last path: {project_path}, {:?}", self.prev_tests);
+        self.prev_tests.get(&project_path).cloned()
+    }
+}
+
+fn exec_test(name: String) {
+    let mut cmd = process::Command::new("cargo");
+    cmd.arg("test").arg(name);
+
+    cmd.exec();
+}
 
 fn main() {
-    let dir = "../rust";
-    let search = "sort_suggested";
+    let cli_data = CliData::init();
+    let wd = std::env::current_dir().unwrap();
+    let wd = wd.file_name().unwrap().to_str().unwrap();
+    let Cli { term: Some(term), .. } = Cli::parse() else {
+        exec_test(cli_data.get_last_test(wd.to_string()).unwrap());
+        return;
+    };
 
+    let dir = Path::new(".");
     let walker = walkdir::WalkDir::new(dir);
     let mut files: Vec<String> = vec![];
     for entry in walker {
@@ -22,21 +99,6 @@ fn main() {
 
     let matcher = RegexMatcher::new(r"\#\[test\]\n\s*fn\s+.+\(").unwrap();
     let mut searcher = SearcherBuilder::new().multi_line(true).build();
-
-    searcher.search_slice(
-        matcher.clone(),
-        "
-          #[test]
-                fn sort_suggested_structs_by_types() {
-        check(
-            r#
-        "
-        .as_bytes(),
-        UTF8(|_, line| {
-            println!("line: {}", line);
-            Ok(true)
-        }),
-    );
 
     let mut scores: Vec<(u8, String, String)> = Vec::new();
 
@@ -52,7 +114,7 @@ fn main() {
                 let name = name.trim();
 
                 scores.push((
-                    fuzzywuzzy::fuzz::ratio(search, name),
+                    fuzzywuzzy::fuzz::partial_ratio(&term, name),
                     name.to_string(),
                     f.clone(),
                 ));
@@ -61,6 +123,28 @@ fn main() {
             }),
         );
     }
+
     scores.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
-    println!("results: {:#?}", scores.split_at(5).0);
+
+    let selection = Select::new()
+        .with_prompt("What do you choose?")
+        .items(
+            &scores
+                .split_at(min(5, scores.len()))
+                .0
+                .iter()
+                .map(|v| v.1.clone())
+                .collect::<Vec<String>>(),
+        )
+        .interact()
+        .unwrap();
+
+    cli_data.save_test(wd.to_string(), scores[selection].1.clone());
+
+    let mut cmd = process::Command::new("cargo");
+    cmd.arg("test").arg(scores[selection].1.clone());
+
+    cmd.exec();
+
+    println!("running_test: {}", scores[selection].1);
 }
